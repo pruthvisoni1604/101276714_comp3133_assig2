6ce962244395e34ea29648d74a083cc51e7d3cb0
pruthvisoni1604 <pruthvisoni1604@gmail.com>
2022-04-10 21:39:15 -0400

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = '101276714_comp3133_assig2';
+}
